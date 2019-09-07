5dbb95152ed69a9102c1c0f4655e5ef2ba165365
mister <mister@DESKTOP-534BRGJ>
2019-09-07 09:51:51 +0530

    test

--- mavendemo2/src/test/java/demo9/Amit6.java
+++ mavendemo2/src/test/java/demo9/Amit6.java
@@ -3,7 +3,7 @@ package demo9;
 public class Amit6 {
 
 	public static void main(String[] args) {
-		System.out.println("this class is added by Aazm");
+		System.out.println("this class is added by Aazm1");
 
 	}
 
